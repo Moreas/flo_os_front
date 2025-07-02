281dac60835dca2a5456644650e28dbec86367b7
Moreas <florent.pignaud@pfj-services.com>
2025-07-02 19:26:00 -0400

    commit

--- src/pages/MeetingsPage.tsx
+++ src/pages/MeetingsPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { apiClient } from '../api/apiConfig';
-import { Card } from '../components/ui/card';
 import { 
   CalendarIcon, 
   UserGroupIcon, 
